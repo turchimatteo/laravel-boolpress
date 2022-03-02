b26e56a688b899e797ea9aa873c66ccd9e5286c4
turchimatteo <turchi12@gmail.com>
2022-03-02 09:13:40 +0100

    category e user

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <h1>Dashboard</h1>
+    <section>
+        <div class="container">
+            <h1>Benvenuto {{ $user->name }} nell'area protetta</h1>
+    
+            @if ($userInfo)
+                <div>Telefono: {{ $userInfo->phone }}</div>
+                <div>Indirizzo: {{ $userInfo->address }}</div>
+            @endif
+        </div>
+    </section>
 @endsection
